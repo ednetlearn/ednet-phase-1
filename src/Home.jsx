751ba13f04de67fde70a3ce8eaa6c256359cb7e7
aadhyan <aadhyan@APPLEs-MacBook-Air.local>
2025-07-01 11:12:36 +0530

    Update Home page with last updated info

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { lastUpdated } from './lastUpdated';  // Import lastUpdated
 
-const Home = () => {
-  return (
-    <div>
-      <h2>Home</h2>
-      <p>Welcome to the EdNet application!</p>
-      <p>Use the navigation links above to explore.</p>
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+    <p>Welcome to the EdNet application!</p>
+    <p>Use the navigation links above to explore.</p>
+    <p style={{ marginTop: '1.5em', fontStyle: 'italic', color: '#555' }}>
+      Last Updated: {lastUpdated}
+    </p>  {/* Display Last Updated info */}
+  </div>
+);
 
 export default Home;
