5432b6d66f23d02e5aa1c524e66afae82fb3e815
ednetlearn <seshakalpana7@gmail.com>
2025-07-02 16:40:28 +0530

    Update ChessGame.jsx

--- src/src/games/ChessGame.jsx
+++ src/src/games/ChessGame.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+
+export default function ChessGame() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Chess Game</h2>
+      <p>This is where the Chess game will be developed.</p>
+    </div>
+  );
+}
 
