f6d0d4ed85bf9bcc762da8fef24e944cbe28b112
ednetlearn <seshakalpana7@gmail.com>
2025-07-02 16:49:45 +0530

    Update CodingPuzzle.jsx

--- src/src/games/CodingPuzzle.jsx
+++ src/src/games/CodingPuzzle.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+
+export default function CodingPuzzle() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Coding Puzzle</h2>
+      <p>This is where coding puzzles and challenges will appear.</p>
+    </div>
+  );
+}
 
