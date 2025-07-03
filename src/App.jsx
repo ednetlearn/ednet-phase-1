c657ec83cad4e364942255eb22237a46179ff53d
ednetlearn <seshakalpana7@gmail.com>
2025-07-03 12:22:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import CombinedWorksheetGenerator from './CombinedWorksheetGenerator';
+import Home from './Home';
 import GamesPage from './GamesPage';
+import WorksheetGenerator from './WorksheetGenerator';
 import ScanMyBook from './ScanMyBook';
+import SyllabusUploader from './SyllabusUploader';  // Import the new component
 
 function App() {
   return (
     <Router>
       <nav style={{ padding: 10, backgroundColor: '#eee', marginBottom: 20 }}>
-        <Link to="/" style={{ marginRight: 15 }}>Games</Link>
+        <Link to="/" style={{ marginRight: 15 }}>Home</Link>
+        <Link to="/games" style={{ marginRight: 15 }}>Games</Link>
         <Link to="/worksheets" style={{ marginRight: 15 }}>Worksheet Generator</Link>
-        <Link to="/scan">Scan My Book</Link>
+        <Link to="/scan" style={{ marginRight: 15 }}>Scan My Book</Link>
+        <Link to="/upload-syllabus" style={{ marginRight: 15 }}>Upload Syllabus</Link> {/* New link */}
       </nav>
 
       <Routes>
-        <Route path="/" element={<GamesPage />} />
-        <Route path="/worksheets" element={<CombinedWorksheetGenerator />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/worksheets" element={<WorksheetGenerator />} />
         <Route path="/scan" element={<ScanMyBook />} />
+        <Route path="/upload-syllabus" element={<SyllabusUploader />} />  {/* New route */}
       </Routes>
     </Router>
   );
