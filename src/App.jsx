28eaf951c421e24b6c823a67ccb4cbdc0ab1d43e
ednetlearn <seshakalpana7@gmail.com>
2025-07-04 23:36:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import ParentPage from './pages/ParentPage';
-import Register from './pages/Register';
 import SamplesPreview from './pages/SamplesPreview';
 
 function App() {
   return (
     <Router>
+      <nav style={{ padding: '1rem' }}>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/parent">Parent Page</Link> |{' '}
+        <Link to="/samples">Samples Preview</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/parent" element={<ParentPage />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/samples" element={<SamplesPreview />} />
       </Routes>
     </Router>
