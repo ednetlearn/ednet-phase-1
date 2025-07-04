ef24c4796286d5a5cf657d802708940501014276
ednetlearn <seshakalpana7@gmail.com>
2025-07-04 19:12:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./Home";
-import GamesPage from "./GamesPage";
-import WorksheetGenerator from "./WorksheetGenerator";
-import ScanAndGameZoneUI from "./ScanAndGameZoneUI";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import GamesPage from './Pages/GamesPage';
+import WorksheetGenerator from './Pages/WorksheetGenerator';
+import ScanAndGameZoneUI from './Pages/ScanAndGameZoneUI';
+import LoginAccount from './Pages/LoginAccount';
 
 function App() {
   return (
     <Router>
-      <nav style={{ padding: "10px", background: "#e8f4ff" }}>
-        <Link to="/" style={{ margin: "0 10px" }}>Home</Link>
-        <Link to="/games" style={{ margin: "0 10px" }}>Games</Link>
-        <Link to="/worksheets" style={{ margin: "0 10px" }}>Worksheet Generator</Link>
-        <Link to="/scan" style={{ margin: "0 10px" }}>Scan & Game Zone</Link>
+      <nav>
+        <Link to="/">Home</Link> |{' '}
+        <Link to="/games">Games</Link> |{' '}
+        <Link to="/worksheets">Worksheet Generator</Link> |{' '}
+        <Link to="/scan">Scan & Game Zone</Link> |{' '}
+        <Link to="/login">Login</Link>
       </nav>
 
       <Routes>
@@ -20,10 +22,10 @@ function App() {
         <Route path="/games" element={<GamesPage />} />
         <Route path="/worksheets" element={<WorksheetGenerator />} />
         <Route path="/scan" element={<ScanAndGameZoneUI />} />
+        <Route path="/login" element={<LoginAccount />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
