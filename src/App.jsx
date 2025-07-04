64325914d15ec4b589280316967b274729cce5ad
ednetlearn <seshakalpana7@gmail.com>
2025-07-04 23:58:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import ParentPage from './pages/ParentPage';
-import SamplesPreview from './pages/SamplesPreview';
 
 function App() {
   return (
-    <Router>
-      <nav style={{ padding: '1rem' }}>
-        <Link to="/">Home</Link> |{' '}
-        <Link to="/parent">Parent Page</Link> |{' '}
-        <Link to="/samples">Samples Preview</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/parent" element={<ParentPage />} />
-        <Route path="/samples" element={<SamplesPreview />} />
-      </Routes>
-    </Router>
+    <div style={{ textAlign: 'center' }}>
+      <h1>Welcome to EdNet</h1>
+      <img src="/logo.png" alt="EdNet Logo" width="200" />
+    </div>
   );
 }
 
